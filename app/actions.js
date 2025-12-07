88c57f088ec2f3e44f7bd418330a54cdb294ca2a
mattbertrand <matthieubertrand5@yahoo.fr>
2025-12-06 21:58:15 -0800

    handling of object streams

--- app/actions.js
+++ app/actions.js
@@ -1,26 +1,36 @@
 "use server";
 
-import { generateObject } from "ai";
+import { streamObject } from "ai";
 import { openai } from "@ai-sdk/openai";
+import { createStreamableValue } from "@ai-sdk/rsc";
 import { z } from "zod";
 
-export async function getData(input) {
-    "use server"
-    const {object: people} = await generateObject({
-        model: openai("gpt-5.1"),
-        system: "You are a helpful assistant that generates lists of famous travelers.",
-        prompt: input,
-        schema: z.object({
-            people: z.array(
-                z.object({
-                    name: z.string().describe("The full name of the traveler"),
-                    knownFor: z.string().describe("What the traveler is known for"),
-                    address: z.string().describe("The traveler's primary location"),
-                    age: z.number().describe("The age of the traveler")
-                })
-            )
-        })
-    });
+export async function generate(input) {
+    "use server";
 
-    return {people};
+    const stream = createStreamableValue();
+
+    (async () => {
+        const {partialObjectStream} = await streamObject({
+                model: openai("gpt-5.1"),
+                system: "You are a helpful assistant that generates lists of famous travelers.",
+                prompt: input,
+                schema: z.object({
+                    people: z.array(
+                        z.object({
+                            name: z.string().describe("The full name of the traveler"),
+                            knownFor: z.string().describe("What the traveler is known for"),
+                            address: z.string().describe("The traveler's primary location"),
+                            age: z.number().describe("The age of the traveler")
+                        })
+                    )
+                })
+            });
+            for await (const partialObject of partialObjectStream) {
+                stream.update(partialObject);
+            }
+            stream.done();
+        })();
+    return {object: stream.value};
+
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 "use client";
 
+import { generate } from "./actions";
 import { useState } from "react";
-import { getData } from "./actions";
+import { readStreamableValue } from "@ai-sdk/rsc";
 
 export const dynamic = "force-dynamic";
 export const maxDuration = 30;
@@ -10,13 +11,19 @@ export default function Home() {
   const [generation, setGeneration] = useState("");
 
   return (
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black">
-        <button onClick={async () => {
-          const { people } =  await getData("Generate a list of 5 famous travelers including their name, what they are known for, address, and age.");
-          setGeneration(JSON.stringify(people, null, 2));
-        }}
-        >View People!</button>
-        <pre className="mt-8 whitespace-pre-wrap">{generation}</pre>
-      </main>
+    <div>
+      <button
+        onClick={async () => {
+          const {object} = await generate(  
+            "Generate a list of famous travelers with their name, what they are known for, address, and age."
+          );
+          for await (const partialObject of readStreamableValue(object)) {
+            if(partialObject) {
+              setGeneration(JSON.stringify(partialObject, null, 2));
+            }
+          }
+        }}>View People!</button>
+        <pre>{generation}</pre>
+    </div>
   );
 }
